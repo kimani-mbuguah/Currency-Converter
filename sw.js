62b41edae2fc17b6d047703dfaba98b9b58cf9db
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-06-29 15:30:06 +0300

    commit three

---
+++ sw.js
@@ -0,0 +1,41 @@
+let staticCacheName = 'curr-cnv-static-cache-v1';
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        'index.html',
+        'https://fonts.googleapis.com/css?family=Montserrat',
+        'js/app.js',
+        'js/sweetalert/sweetalert.min.js',
+        'css/sweetalert/sweetalert.css',
+        'css/styles.css',
+        'images/money.jpg',
+        'images/favicon.ico'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('curr-cnv--') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
